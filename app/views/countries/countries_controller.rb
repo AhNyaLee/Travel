bc3363b9acc763ae6a365e4b3ac0dfdde6a559b7
AhNyaLee <jana.polya@yandex.ru>
2025-01-15 23:21:51 +0300

    created countries  views

---
+++ app/views/countries/countries_controller.rb
@@ -0,0 +1,41 @@
+class CountriesController < ApplicationController
+  before_action :set_country, only: %i[update show destroy edit]
+
+  def create
+    country=Country.create(country_params )
+    redirect_to "/countries/#{country.id})"
+  end
+   
+  def update
+    @country.update(country_params)
+    redirect_to "/countries/#{@country.id}"
+  end
+  
+  def destroy
+    @country.destroy
+    redirect_to "/countries"
+  end 
+  
+  def show 
+  end
+
+  def index
+    @country=Country.all
+  end
+  
+  def new 
+    @country=Country.new
+  end  
+  
+  def edit
+  end  
+
+  private
+  def country_params
+    params.require(:country).permit(:country_code,:name, :language,:currency,:timezone,:capital)
+  end  
+
+  def set_country
+    @country=Country.find(params[:id])
+  end  
+end
